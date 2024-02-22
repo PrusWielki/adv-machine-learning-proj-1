804c06d3d0b52f9eacaed0c3d14c99f19e7416c7
Patryk <pruswielki@gmail.com>
2024-02-22 19:54:07 +0100

    skeleton

---
+++ solution.py
@@ -0,0 +1,30 @@
+class LDA:
+    def fit(X,y):
+        return NotImplementedError
+    def predict_proba(Xtest):
+        return NotImplementedError
+    def predict(Xtest):
+        return NotImplementedError
+    def get_params():
+        return NotImplementedError
+    
+class QDA:
+    def fit(X,y):
+        return NotImplementedError
+    def predict_proba(Xtest):
+        return NotImplementedError
+    def predict(Xtest):
+        return NotImplementedError
+    def get_params():
+        return NotImplementedError
+    
+
+class NB:
+    def fit(X,y):
+        return NotImplementedError
+    def predict_proba(Xtest):
+        return NotImplementedError
+    def predict(Xtest):
+        return NotImplementedError
+    def get_params():
+        return NotImplementedError
