8e91904ba748656cf2b4e5bf777979aebd046e21
Patryk <pruswielki@gmail.com>
2024-02-22 20:51:01 +0100

    progress

--- solution.py
+++ solution.py
@@ -5,6 +5,22 @@ class LDA:
     def fit(X,y):
         print(X)
         print(y)
+        print("\n")
+        print(X[y==1])
+        # 1. Calculate the between-class variance
+        # 2. Calculate the within-class variance
+        # 3. Compute the eigenvectors and the corresponding eigenvalues.
+        # 4. Put the eigenvalues in decreasing order and select k eigenvectors with the largest eigenvalues.
+        # 5. Create a k dimensional matrix containing the eigenvectors.
+        number_of_features = len(X[0])
+        Sw = np.zeros((number_of_features,number_of_features))
+        Sb = np.zeros((number_of_features,number_of_features))
+
+        for i in range(0,1):
+            return NotImplementedError
+
+
+
         return NotImplementedError
     def predict_proba(Xtest):
         return NotImplementedError
