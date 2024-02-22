9cbcfd3e435f1ff5d91733fd63675beaf54c4307
Patryk <pruswielki@gmail.com>
2024-02-22 20:37:41 +0100

    testdata

--- solution.py
+++ solution.py
@@ -1,5 +1,10 @@
+import numpy as np
+
+
 class LDA:
     def fit(X,y):
+        print(X)
+        print(y)
         return NotImplementedError
     def predict_proba(Xtest):
         return NotImplementedError
@@ -28,3 +33,13 @@ class NB:
         return NotImplementedError
     def get_params():
         return NotImplementedError
+    
+
+
+def main():
+    testDataSet = np.random.random((5, 10))
+    testYVariables = np.random.randint(2,size=5)
+    LDA.fit(testDataSet,testYVariables)
+
+if __name__ == "__main__":
+    main()
